ce0a8eccee570006ff398785f1862f45f8ba97c2
Mikhail Tabakaev <47657224+Mihakgma@users.noreply.github.com>
2024-06-07 00:14:29 +0700

    in second test solution some final additions and fixes: coding in all files changed to utf-16, bug in HouseMarker class has been fixed (problem with adding Room obj to Set), also added javadoc for current solution

---
+++ JAVA/second_test_solution/javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"FamilyFinder"},{"p":"<Unnamed>","l":"House"},{"p":"<Unnamed>","l":"HousesMarker"},{"p":"<Unnamed>","l":"Humans"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Menu"},{"p":"<Unnamed>","l":"Room"}];updateSearchResults();
